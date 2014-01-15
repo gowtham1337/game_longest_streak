0e5b981a04f72476d76ad4f284e267facb9da01b
gowtham <gowtham@milaap.org>
2014-01-15 16:57:41 +0530

    Update Log

--- game_longest_streak.rb
+++ game_longest_streak.rb
@@ -37,7 +37,7 @@ def update_cron(gap)
   File.open("#{File.expand_path( File.dirname( __FILE__ ))}/cron", "w+") do |f|
     f.puts("#{next_time.strftime("%M %H %d %m")} * ruby #{Dir.pwd}/#{__FILE__}\n")
   end
-  system("crontab cron")
+  system("cd #{$curr_dir} && crontab cron")
 end
 
 $curr_time = Time.now
